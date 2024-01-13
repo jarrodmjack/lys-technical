699b681acfa3321124dfe5f15a88c385d26d41d8
jarrodmjack <jarrodmjack@gmail.com>
2024-01-12 20:29:16 -0500

    add times visited

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,16 +6,15 @@ import ComicNavigationBar from "../components/ComicNavigationBar";
 export async function loader() {
     const response = await fetch(`${import.meta.env.VITE_API_URL}`);
     const data = await response.json();
-    console.log('data: ', data)
     return data;
 }
 
 const Home = () => {
     const data = useLoaderData();
-    console.log('data: ', data)
 
     return (
         <>
+            <h1>{data.timesVisited} visits</h1>
             <ComicNavigationBar comicNum={data.comic.num} />
             <Comic comic={data.comic} />;
         </>
